dad23ce5d3eaa6bffd7dc17ccc3b4bb7acc25204
liboxi <liboxi@zbj.com>
2018-03-27 17:26:09 +0800

    up

--- spring-cloud-eureka-client/src/main/java/hello/EurekaClientApplication.java
+++ spring-cloud-eureka-client/src/main/java/hello/EurekaClientApplication.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,28 +10,33 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaClientApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+}
+
+@RestController
+@RequestMapping("/hello")
+class HelloController {
+	@RequestMapping(value = "get", method = RequestMethod.GET)
+	public String get() {
+		return "get ok";
+	}
 }
 
 @RestController
 class ServiceInstanceRestController {
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
-            @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
-    }
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
 }
